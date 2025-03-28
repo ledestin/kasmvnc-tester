1b2223dd1613de95235e2eb1b4ac289677d022b4
Dmitry Maksyoma <ledestin@gmail.com>
2025-03-28 19:41:46 +1300

    KASM-7052 Refactor

--- test.py
+++ test.py
@@ -11,6 +11,23 @@ def find_container_for_image(image):
     if container.attrs['Config']['Image'] == image:
         return container
 
+def check_xfce_running(container):
+  try:
+    check_xfce_cmd = '/bin/bash -c "pgrep xfce4-session"'
+    xfce = container.exec_run(check_xfce_cmd)
+    if xfce[0] == 0:
+      pids = xfce[1].decode("utf-8")
+      report_tests.append({'testname':'XFCE Running','status':'pass','errorout': 'XFCE is running pid ' + pids})
+      print('xfce - complete')
+    else:
+      report_tests.append({'testname':'XFCE Running','status':'fail','errorout': 'XFCE is not running'})
+      report_status = 'FAIL'
+      print('xfce - error')
+  except Exception as error:
+    report_tests.append({'testname':'XFCE Running','status':'fail','errorout': error})
+    report_status = 'FAIL'
+    print('xfce - error')
+
 import os
 
 global report_status
@@ -19,25 +36,10 @@ global docker_client
 report_tests = []
 report_status = 'PASS'
 
-TEST_IMAGE = detect_test_image
+TEST_IMAGE = detect_test_image()
 
 import docker
 docker_client = docker.from_env()
 
 test_container = find_container_for_image(TEST_IMAGE)
-try:
-  # Detect the xfce-session pid
-  check_xfce = '/bin/bash -c "pgrep xfce4-session"'
-  xfce = test_container.exec_run(check_xfce)
-  if xfce[0] == 0:
-    pids = xfce[1].decode("utf-8")
-    report_tests.append({'testname':'XFCE Running','status':'pass','errorout': 'XFCE is running pid ' + pids})
-    print('xfce - complete')
-  else:
-    report_tests.append({'testname':'XFCE Running','status':'fail','errorout': 'XFCE is not running'})
-    report_status = 'FAIL'
-    print('xfce - error')
-except Exception as error:
-  report_tests.append({'testname':'XFCE Running','status':'fail','errorout': error})
-  report_status = 'FAIL'
-  print('xfce - error')
+check_xfce_running(test_container)
