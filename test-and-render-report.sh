b4a6ead77732639cba754d0f0fc5239e6a814729
Dmitry Maksyoma <ledestin@gmail.com>
2025-04-07 20:24:23 +1200

    KASM-7052 Detect docker group id

--- test-and-render-report.sh
+++ test-and-render-report.sh
@@ -3,5 +3,7 @@
 set -e
 
 docker build -t kasmvnc-functional-test .
-docker run --rm -v "$PWD":/src  -v /var/run/docker.sock:/var/run/docker.sock --group-add 997 kasmvnc-functional-test \
+docker_group=$(grep docker /etc/group | awk -F: '{ print $3 }')
+docker run --rm -v "$PWD":/src  -v /var/run/docker.sock:/var/run/docker.sock \
+  --group-add "$docker_group" kasmvnc-functional-test \
   bash -c 'nginx & cd /src && /tmp/test-and-render-report'
