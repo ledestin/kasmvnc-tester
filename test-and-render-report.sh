e4fd46ad88214d1f710533234e5bf12e9c5f348f
Dmitry Maksyoma <ledestin@gmail.com>
2025-04-27 21:15:01 +1200

    KASM-7052 Fail if no KASMVNC_IMAGE_TO_TEST_ON is set

--- test-and-render-report.sh
+++ test-and-render-report.sh
@@ -5,5 +5,6 @@ set -e
 docker build -t kasmvnc-functional-test .
 docker_group=$(grep docker /etc/group | awk -F: '{ print $3 }')
 docker run --rm -v "$PWD":/src  -v /var/run/docker.sock:/var/run/docker.sock \
+  -e KASMVNC_IMAGE_TO_TEST_ON \
   --group-add "$docker_group" kasmvnc-functional-test \
   bash -c 'nginx & cd /src && /tmp/test-and-render-report'
