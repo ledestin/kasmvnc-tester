ad2f8d08e1193cc9927cd7e7764e5e71eade4949
Dmitry Maksyoma <ledestin@gmail.com>
2025-04-29 22:22:13 +1200

    KASM-7052 Test in CI

--- test-and-render-report.sh
+++ test-and-render-report.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 docker build -t kasmvnc-functional-test .
-docker_group=$(stat -c '%g' /var/run/docker.sock)
+docker_group=2375
 docker run --rm -v "$PWD":/src  -v /var/run/docker.sock:/var/run/docker.sock \
   -e KASMVNC_IMAGE_TO_TEST_ON \
   --group-add "$docker_group" kasmvnc-functional-test \
