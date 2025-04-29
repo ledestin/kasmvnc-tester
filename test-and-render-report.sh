fbd83bb03b22ce4e05182c323e78f5db9fb811f1
Dmitry Maksyoma <ledestin@gmail.com>
2025-04-29 21:12:33 +1200

    KASM-7052 Debug CI

--- test-and-render-report.sh
+++ test-and-render-report.sh
@@ -8,4 +8,4 @@ docker_group=$(grep docker /etc/group | awk -F: '{ print $3 }')
 docker run --rm -v "$PWD":/src  -v /var/run/docker.sock:/var/run/docker.sock \
   -e KASMVNC_IMAGE_TO_TEST_ON \
   --group-add "$docker_group" kasmvnc-functional-test \
-  bash -c 'nginx & id; groups; grep 2375 /etc/group; ls -l /var/run/docker.sock; cd /src && /tmp/test-and-render-report'
+  bash -c 'nginx & id; groups; tail /etc/group; ls -l /var/run/docker.sock; cd /src && /tmp/test-and-render-report'
