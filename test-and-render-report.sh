4da13fc5f1620756a788f2ff5a0504bca8ead866
Dmitry Maksyoma <ledestin@gmail.com>
2025-04-29 21:50:17 +1200

    KASM-7052 Improve docker group detection

--- test-and-render-report.sh
+++ test-and-render-report.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 docker build -t kasmvnc-functional-test .
-docker_group=$(grep docker /etc/group | awk -F: '{ print $3 }')
+docker_group=$(stat -c '%g' /var/run/docker.sock)
 docker run --rm -v "$PWD":/src  -v /var/run/docker.sock:/var/run/docker.sock \
   -e KASMVNC_IMAGE_TO_TEST_ON \
   --group-add "$docker_group" kasmvnc-functional-test \
